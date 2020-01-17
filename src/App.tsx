44e34f5cb45decc0c2d9b50ed046bc014ce5ee09
Lytvyn, Mykolai <>
2020-01-17 17:39:55 +0200

    In progress.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,53 @@
 import React from 'react';
 import './App.css';
 
+// material ui
+import {Grid, Typography, NoSsr, makeStyles, Paper} from '@material-ui/core';
+
 // routing
 import Routing from './pages/routing';
 
-const App: React.FC = () => {
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 
+const App: React.FC = () => {
+    const classes = useStyles();
 
     return (
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                {/*header*/}
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>xs=12</Paper>
+                </Grid>
 
-        <div className="App">
-            <div>must works</div>
-            <Routing/>
+                <Grid item xs={2}>
+                    <Paper className={classes.paper}>xs=6</Paper>
+                </Grid>
+
+                {/*Routing*/}
+                <Grid item xs={8}>
+                    <Paper className={classes.paper}>
+                        {/*routing*/}
+                        <Routing/>
+                    </Paper>
+                </Grid>
+
+                <Grid item xs={2}>
+                    <Paper className={classes.paper}>xs=6</Paper>
+                </Grid>
+
+            </Grid>
         </div>
     );
-}
+};
 
 export default App;
