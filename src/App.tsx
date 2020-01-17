c74a8845ad41e4576a6dbf0c947f299c38076f5e
Lytvyn, Mykolai <>
2020-01-17 15:58:51 +0200

    In progress.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './App.css';
 
-// todo ... Mykolai Lytvyn ... Will be discussed
-import {request as GamesRequest} from './commons/api/games';
-import {request as PlayersRequest} from './commons/api/players';
-import {request as TeamsRequest} from './commons/api/teams';
+// routing
+import Routing from './pages/routing';
 
 const App: React.FC = () => {
 
-    useEffect(() => {
-        wrapperFunction();
-    });
-
-    const wrapperFunction = async () => {
-        const gamesResult = await GamesRequest();
-        console.log(' gamesResult:', gamesResult);
-
-        const playersResult = await PlayersRequest();
-        console.log(' playersResult:', playersResult);
-
-        const teamsResult = await TeamsRequest();
-        console.log(' teamsResult:', teamsResult);
-
-    };
 
     return (
+
         <div className="App">
-            {/*todo... will be rmeoved*/}
-            <div>aa</div>
+            <div>must works</div>
+            <Routing/>
         </div>
     );
 }
