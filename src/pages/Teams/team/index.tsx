f47240c00afd151bd2ca1753aafd1f97c7af990a
thismytests <iunknowunnamed@gmail.com>
2020-01-19 18:08:35 +0200

    In progress.

--- src/pages/Teams/team/index.tsx
+++ src/pages/Teams/team/index.tsx
@@ -1,16 +1,29 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
+import {Avatar, Grid} from "@material-ui/core";
+
+// routing
+import {useRelocateToTeamsInfo} from '../../hooks';
 
 export interface TeamProps {
-    id: string,
-    name: string,
-    logo: string
+  id: string,
+  name: string,
+  logo: string
 }
 
 export default function Team(props: TeamProps) {
-    const {id, name, logo} = props;
-    return (
-        <div>
-            {logo} - {id} - {name}
-        </div>
-    )
+  const {id, name, logo} = props;
+  const [makeRelocate] = useRelocateToTeamsInfo();
+
+  const relocateToAnotherPage = () => {
+    makeRelocate(id)
+  };
+
+  return (
+    <Grid item container onClick={relocateToAnotherPage}>
+      <Grid item xs={2}>
+        <Avatar alt="Remy Sharp" src={logo}/>
+      </Grid>
+      <Grid item xs={10} alignContent={'flex-start'}>{name}</Grid>
+    </Grid>
+  )
 }
