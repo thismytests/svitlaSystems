83e47e044841f872c130ba16abac466432323fa9
Lytvyn, Mykolai <>
2020-01-21 18:03:01 +0200

    Move to table

--- src/pages/TeamsInfo/comonents/playersInfo/index.tsx
+++ src/pages/TeamsInfo/comonents/playersInfo/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import {Grid} from "@material-ui/core";
+import {Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
 
 // types
 import {PlayersInfoProps} from './types';
 import Item from './player';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name: any, calories: any, fat: any, carbs: any, protein: any) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
 export default function PlayersInfo(props: { data: Array<PlayersInfoProps> | undefined }) {
+  const classes = useStyles();
+
   const createPlayersTemplate = (): any => {
     if (props.data === undefined) {
       return null
@@ -22,7 +44,34 @@ export default function PlayersInfo(props: { data: Array<PlayersInfoProps> | und
 
   return (
     <>
-      {createPlayersTemplate()}
+      {/*{createPlayersTemplate()}*/}
+
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
