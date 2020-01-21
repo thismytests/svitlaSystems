8c17d51178f01b65712245ce6be35ca18a99c1e4
Lytvyn, Mykolai <>
2020-01-21 17:42:37 +0200

    In progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,14 @@ import React from 'react';
 import './App.css';
 
 // material ui
-import {Grid, makeStyles} from '@material-ui/core';
+import {Grid} from '@material-ui/core';
 
 // routing
 import Routing from './pages/routing';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
-
 const App: React.FC = () => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <div>
       <Grid container spacing={3}>
         {/*Routing*/}
         <Grid item sm={12}>
